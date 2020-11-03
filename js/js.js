7820ae6c650346e48724cc2f00cb3bf3e2d79f8e
unknown <juan.bueno01@correo.usa.edu.co>
2020-11-03 11:29:32 -0500

    arreglos registro

--- js/js.js
+++ js/js.js
@@ -1,3 +1,21 @@
 $("#admin").click( function(){
     $('#admi').toggle('slow');
 })
+$(function(){
+    tabla();
+});
+
+function tabla(){
+    $.ajax({
+        utl:'consulta.py',
+        type: 'POST',
+        success: function(res){
+            var js= JSON.parse(res);
+            var tabla;
+            for(var i = 0;i<js.length;i++ ){
+                tabla += '<tr><td>'+js[i].numero_orden+'</td><td>'+js[i].fecha+'</td><td>'+'</td><td>'+js[i].cliente_documento+'</td><td>'+'</td><td>'+js[i].empaquetado+'</td><td>'+'</td><td>'+js[i].domicilio+'</td><td>'+'</td><td>'+js[i].vendedor+'</td></tr>';
+            }
+            $('#tbody').html(tabla);
+        }
+    })
+}
