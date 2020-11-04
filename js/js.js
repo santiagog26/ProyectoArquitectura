42ae80767e40c8a08abe7a666799379fcccbd235
unknown <juan.bueno01@correo.usa.edu.co>
2020-11-03 20:11:40 -0500

    abla

--- js/js.js
+++ js/js.js
@@ -1,23 +1,3 @@
 $("#admin").click( function(){
     $('#admi').toggle('slow');
 })
-$(function(){
-   tabla();
-});
-
-function tabla(){
-   $.ajax({
-        url:'/cgi-bin/ProyectoArquitecturaBack/tabla.py',
-        type:'POST',
-        contentType: "text/json; charset=utf-8",
-        dataType: "JSON",
-        success: function(res){
-            var js= JSON.parse(res);
-            var tabla;
-            for(var i = 0;i<js.length;i++ ){
-                tabla += '<tr><td>'+js[i].numero_orden+'</td><td>'+js[i].fecha+'</td><td>'+'</td><td>'+js[i].cliente_documento+'</td><td>'+'</td><td>'+js[i].empaquetado+'</td><td>'+'</td><td>'+js[i].domicilio+'</td><td>'+'</td><td>'+js[i].vendedor+'</td></tr>';
-            }
-            $('#t').html(tabla);
-        }
-    })
-}
