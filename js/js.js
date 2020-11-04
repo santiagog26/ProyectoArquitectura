80424fc92101da15c6df1ed44b0bbb5c21d01f81
unknown <juan.bueno01@correo.usa.edu.co>
2020-11-03 19:13:28 -0500

    arreglo

--- js/js.js
+++ js/js.js
@@ -1,4 +1,20 @@
 $("#admin").click( function(){
     $('#admi').toggle('slow');
 })
+//$(function(){
+  //  tabla();
+//});
 
+//function tabla(){
+  //  $.ajax({
+    //    url:'/cgi-bin/ProyectoArquitecturaBack/consulta.py',
+      ////success: function(res){
+          //  var js= JSON.parse(res);
+            //var tabla;
+            //for(var i = 0;i<js.length;i++ ){
+              //  tabla += '<tr><td>'+js[i].numero_orden+'</td><td>'+js[i].fecha+'</td><td>'+'</td><td>'+js[i].cliente_documento+'</td><td>'+'</td><td>'+js[i].empaquetado+'</td><td>'+'</td><td>'+js[i].domicilio+'</td><td>'+'</td><td>'+js[i].vendedor+'</td></tr>';
+            //}
+            //$('#tbody').html(tabla);
+        //}
+    //})
+//}
