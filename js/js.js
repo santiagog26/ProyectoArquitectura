eb5704fcff406edf7023ce4aa24c53b1dfc5cbc1
unknown <juan.bueno01@correo.usa.edu.co>
2020-11-03 21:31:03 -0500

    abla

--- js/js.js
+++ js/js.js
@@ -1,3 +1,35 @@
 $("#admin").click( function(){
-    $('#admi').toggle('slow');
+    $('#admi').show('slow');
+    $('#ini').hide('slow');
+    $('#vent').hide('slow');
+    $('#emp').hide('slow');
+    $('#domi').hide('slow');
+})
+$("#inicio").click( function(){
+    $('#admi').hide('slow');
+    $('#ini').show('slow');
+    $('#vent').hide('slow');
+    $('#emp').hide('slow');
+    $('#domi').hide('slow');
+})
+$("#ventas").click( function(){
+    $('#admi').hide('slow');
+    $('#ini').hide('slow');
+    $('#vent').show('slow');
+    $('#emp').hide('slow');
+    $('#domi').hide('slow');
+})
+$("#empa").click( function(){
+    $('#admi').hide('slow');
+    $('#ini').hide('slow');
+    $('#vent').hide('slow');
+    $('#emp').show('slow');
+    $('#domi').hide('slow');
+})
+$("#domici").click( function(){
+    $('#admi').hide('slow');
+    $('#ini').hide('slow');
+    $('#vent').hide('slow');
+    $('#emp').hide('slow');
+    $('#domi').show('slow');
 })
