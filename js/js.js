e76cd112fa62c991e8c96b749f3ba76f139754be
unknown <juan.bueno01@correo.usa.edu.co>
2020-11-03 19:33:24 -0500

    arreglo

--- js/js.js
+++ js/js.js
@@ -1,20 +1,23 @@
 $("#admin").click( function(){
     $('#admi').toggle('slow');
 })
-//$(function(){
-  //  tabla();
-//});
+$(function(){
+   tabla();
+});
 
-//function tabla(){
-  //  $.ajax({
-    //    url:'/cgi-bin/ProyectoArquitecturaBack/consulta.py',
-      ////success: function(res){
-          //  var js= JSON.parse(res);
-            //var tabla;
-            //for(var i = 0;i<js.length;i++ ){
-              //  tabla += '<tr><td>'+js[i].numero_orden+'</td><td>'+js[i].fecha+'</td><td>'+'</td><td>'+js[i].cliente_documento+'</td><td>'+'</td><td>'+js[i].empaquetado+'</td><td>'+'</td><td>'+js[i].domicilio+'</td><td>'+'</td><td>'+js[i].vendedor+'</td></tr>';
-            //}
-            //$('#tbody').html(tabla);
-        //}
-    //})
-//}
+function tabla(){
+   $.ajax({
+        url:'/cgi-bin/ProyectoArquitecturaBack/tabla.py',
+        type:'GET',
+        contentType: "text/json; charset=utf-8",
+        dataType: "JSON",
+        success: function(res){
+            var js= JSON.parse(res);
+            var tabla;
+            for(var i = 0;i<js.length;i++ ){
+                tabla += '<tr><td>'+js[i].numero_orden+'</td><td>'+js[i].fecha+'</td><td>'+'</td><td>'+js[i].cliente_documento+'</td><td>'+'</td><td>'+js[i].empaquetado+'</td><td>'+'</td><td>'+js[i].domicilio+'</td><td>'+'</td><td>'+js[i].vendedor+'</td></tr>';
+            }
+            $('#t').html(tabla);
+        }
+    })
+}
