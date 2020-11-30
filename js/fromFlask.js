cb660e2e0131dfd5380aaef0633a571895fbd207
Santiago Gutiérrez Orjuela <santiago.gutierrez02@correo.usa.edu.co>
2020-11-30 10:15:45 -0500

    prueba flask

--- js/fromFlask.js
+++ js/fromFlask.js
@@ -2,22 +2,22 @@ url = 'ec2-54-236-54-145.compute-1.amazonaws.com:5000';
 var losdatos = []
 
 var app = new Vue({
-    el: '#ini',
+    el: '#app',
     data: {
         datos: losdatos,
-        documento: '',
-        contra: '',
-        nombre: '',
-        apellido: '',
-        correo: '',
-        telefono: ''
+        numero_orden: '',
+        fecha: '',
+        cliente_documento: '',
+        empaquetado: '',
+        domicilio: '',
+        vendedor: ''
     },
     created: function () {
         this.f();
     },
     methods: {
         f: function () {
-            axios.get(url + '/get_usuarios')
+            axios.get(url + '/get_pedidos')
                 .then(response => {
                     this.datos = response.data.results;
                     console.log('Get list users', this.datos);
