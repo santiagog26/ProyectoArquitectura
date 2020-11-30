ee611c12dcca4d804639dbe3877e10fe568cd9f5
Santiago Gutiérrez Orjuela <santiago.gutierrez02@correo.usa.edu.co>
2020-11-29 23:23:59 -0500

    Flask get_usuario

---
+++ js/fromFlask.js
@@ -0,0 +1,36 @@
+url = 'ec2-54-236-54-145.compute-1.amazonaws.com:5000';
+var losdatos = []
+
+var app = new Vue({
+    el: '#ini',
+    data: {
+        datos: losdatos,
+        documento: '',
+        contra: '',
+        nombre: '',
+        apellido: '',
+        correo: '',
+        telefono: ''
+    },
+    created: function () {
+        this.f();
+    },
+    methods: {
+        f: function () {
+            axios.get(url + '/get_usuarios')
+                .then(response => {
+                    this.datos = response.data.results;
+                    console.log('Get list users', this.datos);
+                })
+                .catch(error => console.error(error));
+        },
+        clear: function () {
+            this.documento = '';
+            this.contra = '';
+            this.nombre = '';
+            this.apellido = '';
+            this.correo = '';
+            this.telefono: '';
+        }
+    }
+})
