000e88fcba5a1f8867ad3caad1b93c376596f995
Santiago Gutiérrez Orjuela <santiago.gutierrez02@correo.usa.edu.co>
2020-12-01 14:18:07 -0500

    Pruebas

--- js/prueba.js
+++ js/prueba.js
@@ -28,3 +28,25 @@ var app = new Vue({
     }
 })
 
+var app2 = new Vue({
+    el: '#gpc',
+    data: {
+        datos: pedidos_cliente,
+        numero_orden: '',
+        fecha: '',
+        cliente_documento: '',
+        empaquetado: '',
+        domicilio: '',
+        vendedor: ''
+    },
+    methods: {
+        gpedidos_cliente: function () {
+            axios.get(url + '/get_pedidos_cliente',{cliente_documento = this.doc_cli})
+                .then(response => {
+                    this.datos = response.data.results;
+                    console.log('Get list users', this.datos);
+                })
+                .catch(error => console.error(error));
+        }
+    }
+})
