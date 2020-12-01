7571ccaa21b2c596fe4c922b2533cc08e30f046c
Santiago Gutiérrez Orjuela <santiago.gutierrez02@correo.usa.edu.co>
2020-12-01 11:45:41 -0500

    pruebas

---
+++ js/prueba.js
@@ -0,0 +1,28 @@
+url = 'http://ec2-54-236-54-145.compute-1.amazonaws.com:5000';
+var losdatos = []
+
+var app = new Vue({
+    el: '#ini',
+    data: {
+        datos: losdatos,
+        numero_orden: '',
+        fecha: '',
+        cliente_documento: '',
+        empaquetado: '',
+        domicilio: '',
+        vendedor: ''
+    },
+    created: function () {
+        this.f();
+    },
+    methods: {
+        f: function () {
+            axios.get(url + '/get_pedidos')
+                .then(response => {
+                    this.datos = response.data.results;
+                    console.log('Get list users', this.datos);
+                })
+                .catch(error => console.error(error));
+        }
+    }
+})
