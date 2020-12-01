53a1e42f1717831a4a8fdb642a3c2d8cff7c9595
Santiago Gutiérrez Orjuela <santiago.gutierrez02@correo.usa.edu.co>
2020-12-01 12:58:20 -0500

    Pruebas

--- js/prueba.js
+++ js/prueba.js
@@ -1,5 +1,6 @@
 url = 'http://ec2-54-236-54-145.compute-1.amazonaws.com:5000';
 var losdatos = []
+var pedidos_cliente = []
 
 var app = new Vue({
     el: '#ini',
@@ -18,6 +19,35 @@ var app = new Vue({
     methods: {
         f: function () {
             axios.get(url + '/get_pedidos')
+                .then(response => {
+                    this.datos = response.data.results;
+                    console.log('Get list users', this.datos);
+                })
+                .catch(error => console.error(error));
+        }
+    }
+})
+
+var app2 = new Vue({
+    el: '#gpc',
+    data: {
+        datos: pedidos_cliente,
+        numero_orden: '',
+        fecha: '',
+        cliente_documento: '',
+        empaquetado: '',
+        domicilio: '',
+        vendedor: ''
+    },
+    created: function () {
+        this.gpedidos_cliente();
+    },
+    methods: {
+        gpedidos_cliente: function () {
+            const data = {
+                cliente_documento = this.cliente_documento
+            }
+            axios.get(url + '/get_pedidos_cliente',data)
                 .then(response => {
                     this.datos = response.data.results;
                     console.log('Get list users', this.datos);
